e65550a4f1c7e8b85b7f1821e65978aea6e4e59b
yashsehgal <yashsehgal.work@gmail.com>
2021-05-01 20:01:09 +0530

    project card component created (#28)

--- src/Views/Projects/index.js
+++ src/Views/Projects/index.js
@@ -1,137 +1,40 @@
 
 import React from 'react';
 import { Switch, Route, HashRouter, Link } from 'react-router-dom';
+import ProjectDetails from '../../Assets/ProjectDetails.json'
 
 
-const Projects = function Projects() {
-  return (
-    <div className="Projects">
-      {/* Projects Container*/}
+const Projects = function () {
+    return (
+      <div className="Projects">
+        {/* Projects Container*/}
 
-      <div className="Home-ProjectsContainer page-body Container">
-        <h2 className="headline">
-              Our Projects
+        <div className="Home-ProjectsContainer Container">
+          <h2 className="headline">
+            Our Projects
           </h2>
-        <div className="Home-ProjectsLayer">
-          
-          {/* Card Component */}
-          <div className="ProjectCard">
-            <div className="ProjectCard-ImageContainer">
-              {/* Image for the Project*/}
-            </div>
-            <div className="ProjectCard-CardBody">
-              <p className="ProjectCard-title">
-                Eccentric Touch
-              </p>
-              <p className="ProjectCard-Description">
-                Suffering from Frontend crises? Here's something that might interest you. Eccentric Touch is a small CSS Library to solve all your frontend issues :)
-              </p>
-
-              <button className="PrimaryButton">
-                Learn More
-              </button>
-            </div>
-          </div>
-
-          {/* Card Component */}
-          <div className="ProjectCard">
-            <div className="ProjectCard-ImageContainer">
-              {/* Image for the Project*/}
-            </div>
-            <div className="ProjectCard-CardBody">
-              <p className="ProjectCard-title">
-                Eccentric Touch
-              </p>
-              <p className="ProjectCard-Description">
-                Suffering from Frontend crises? Here's something that might interest you. Eccentric Touch is a small CSS Library to solve all your frontend issues :)
-              </p>
-
-              <button className="PrimaryButton">
-                Learn More
-              </button>
-            </div>
-          </div>
-
-          {/* Card Component */}
-          <div className="ProjectCard">
-            <div className="ProjectCard-ImageContainer">
-              {/* Image for the Project*/}
-            </div>
-            <div className="ProjectCard-CardBody">
-              <p className="ProjectCard-title">
-                Eccentric Touch
-              </p>
-              <p className="ProjectCard-Description">
-                Suffering from Frontend crises? Here's something that might interest you. Eccentric Touch is a small CSS Library to solve all your frontend issues :)
-              </p>
-
-              <button className="PrimaryButton">
-                Learn More
-              </button>
-            </div>
-          </div>
-
-        </div>
-        <div className="Home-ProjectsLayer">
-
-
-        {/* Card Component */}
-          <div className="ProjectCard">
-            <div className="ProjectCard-ImageContainer">
-              {/* Image for the Project*/}
-            </div>
-            <div className="ProjectCard-CardBody">
-              <p className="ProjectCard-title">
-                Eccentric Touch
-              </p>
-              <p className="ProjectCard-Description">
-                Suffering from Frontend crises? Here's something that might interest you. Eccentric Touch is a small CSS Library to solve all your frontend issues :)
-              </p>
-
-              <button className="PrimaryButton">
-                Learn More
-              </button>
-            </div>
-          </div>
-
-          {/* Card Component */}
-          <div className="ProjectCard">
-            <div className="ProjectCard-ImageContainer">
-              {/* Image for the Project*/}
-            </div>
-            <div className="ProjectCard-CardBody">
-              <p className="ProjectCard-title">
-                Eccentric Touch
-              </p>
-              <p className="ProjectCard-Description">
-                Suffering from Frontend crises? Here's something that might interest you. Eccentric Touch is a small CSS Library to solve all your frontend issues :)
-              </p>
-
-              <button className="PrimaryButton">
-                Learn More
-              </button>
-            </div>
-          </div>
-
-          {/* Card Component */}
-          <div className="ProjectCard">
-            <div className="ProjectCard-ImageContainer">
-              {/* Image for the Project*/}
-            </div>
-            <div className="ProjectCard-CardBody">
-              <p className="ProjectCard-title">
-                Eccentric Touch
-              </p>
-              <p className="ProjectCard-Description">
-                Suffering from Frontend crises? Here's something that might interest you. Eccentric Touch is a small CSS Library to solve all your frontend issues :)
-              </p>
-
-              <button className="PrimaryButton">
-                Learn More
-              </button>
-            </div>
-          </div>
-
+          <div className="Home-ProjectsLayer">
+            {ProjectDetails.map((data, key) => {
+              return (
+                <div className="ProjectCard"
+                  key={key}>
+                    <p className="ProjectCard-title">
+                      {data.project_title}
+                    </p>
+                    <p className="ProjectCard-description">
+                      {data.project_description}
+                    </p>
+                    <div className="ProjectCard-bottomLayer">
+                      <button className="ProjectCard-Button">
+                        Learn More
+                      </button>
+                      <p className="ProjectCard-TechStack">
+                        {data.project_stack}
+                      </p>
+                    </div>
+                </div>
+              )
+            })};
         </div>
       </div>
 
